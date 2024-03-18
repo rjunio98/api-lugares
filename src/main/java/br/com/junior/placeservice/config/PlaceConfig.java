632de5bd4f4d9f160102c9a7657690b85f1c05e5
rjunio98 <robledorjunior1@gmail.com>
2024-03-18 02:32:18 -0300

    Adiciona anotações de auditoria no domínio Place e na configuração do PlaceService

--- src/main/java/br/com/junior/placeservice/config/PlaceConfig.java
+++ src/main/java/br/com/junior/placeservice/config/PlaceConfig.java
@@ -2,11 +2,13 @@ package br.com.junior.placeservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 import br.com.junior.placeservice.domain.PlaceRepository;
 import br.com.junior.placeservice.domain.PlaceService;
 
 @Configuration
+@EnableR2dbcAuditing
 public class PlaceConfig {
     
     @Bean
--- src/main/java/br/com/junior/placeservice/domain/Place.java
+++ src/main/java/br/com/junior/placeservice/domain/Place.java
@@ -2,6 +2,9 @@ package br.com.junior.placeservice.domain;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
-public record Place(Long id, String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt) { 
+
+public record Place(Long id, String name, String slug, String state, @CreatedDate LocalDateTime createdAt, @LastModifiedDate LocalDateTime updatedAt) { 
 }
